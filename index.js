0889d7043ae668dbac1f0b14b2023c67c773276d
VASAPON OPASWATANAKUL <vasapon.o@ktc.co.th>
2023-12-06 14:30:33 +0700

    ทำ web app กำหนด 2 URL

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
-console.log("สวัสดี");
+
+app.get('/', function (req, res) {
+  res.send('Hello World')
+})
+
+app.get('/register', function (req, res) {
+    res.send('Register Page')
+  })
+
+app.listen(port,()=>{
+console.log(`Server is running on port ${port}.`)
+}) 
